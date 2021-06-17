f04423c775e6d70e5503ad38bd7b6d08ebc47cdd
marcosaureliodiasmoura <marcos_moura@live.com>
2021-06-17 08:26:56 -0300

    initial project

---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.main}
+`;
+
+export const Title = styled.Text`
+
+`;
